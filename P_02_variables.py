17879445598a3ba28ad0668278bc786aa9b8576d
thompsond50263-svg <thompsond50263@masseyhigh.school.nz>
2026-03-05 17:34:06 +1300

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,17 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the spech marks)
+greeting = "Hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_num = 7
+
